f60e4e769d78e1e092a60925ef68ccfae8db906a
Will Richards <will.f.richards@gmail.com>
2025-03-20 16:44:49 -0400

    updated package.json to launch both windows on launch and fixed script file

--- setup/start_webui.sh
+++ setup/start_webui.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+CURRENT_DIR="$(pwd)"
 
-if [ ! -e "node_modules" ]; then
+if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
+    echo "Please run this script from '$SCRIPT_DIR'"
+    exit 1
+fi
+if [ ! -e "../node_modules" ]; then
     echo "nodes modules not installed, installing now"
     npm i --legacy-peer-deps
 fi
-
 echo "node modules installed, starting web ui server"
-npm run dev
+npm run open
+
