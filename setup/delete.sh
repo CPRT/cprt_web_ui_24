2303ba47855e9aaf8246dd717e7b11ce0de0dba7
Will Richards <will.f.richards@gmail.com>
2025-03-22 10:03:46 -0400

    changes to package json and startup script

---
+++ setup/delete.sh
@@ -0,0 +1,2 @@
+sudo rm /etc/systemd/system/start_webui.service
+# sudo rm /usr/local/bin/start_webui.sh
--- setup/start_webui.sh
+++ setup/start_webui.sh
@@ -2,10 +2,12 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 CURRENT_DIR="$(pwd)"
 
+
 if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
-    echo "Please run this script from '$SCRIPT_DIR'"
-    exit 1
+    echo "Moving to '$SCRIPT_DIR' to execute"
+    cd $SCRIPT_DIR
 fi
+# export NODE_ENV=development
 if [ ! -e "../node_modules" ]; then
     echo "nodes modules not installed, installing now"
     npm i --legacy-peer-deps
