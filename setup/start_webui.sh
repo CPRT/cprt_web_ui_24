193bdb07540e376514591f7661b784ef15b6d883
Will Richards <will.f.richards@gmail.com>
2025-03-22 11:44:27 -0400

    fixed issues with npm

--- setup/start_webui.sh
+++ setup/start_webui.sh
@@ -2,6 +2,17 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 CURRENT_DIR="$(pwd)"
 
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 20
+nvm use 20
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if [ "$SCRIPT_DIR" != "$CURRENT_DIR" ]; then
     echo "Moving to '$SCRIPT_DIR' to execute"
--- setup/start_webui_service.sh
+++ setup/start_webui_service.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 sudo cp start_webui.service /etc/systemd/system/start_webui.service
 sudo sed -i "s|User=%i|User=${CURRENT_USER}|" /etc/systemd/system/start_webui.service
-sudo sed -i "s|ExecStart=%i|ExecStart=${SCRIPT_DIR}/start_webui.sh|" /etc/systemd/system/start_webui.service
+sudo sed -i "s|ExecStart=%i|ExecStart=bash ${SCRIPT_DIR}/start_webui.sh|" /etc/systemd/system/start_webui.service
 sudo chmod 644 /etc/systemd/system/start_webui.service
 # sudo ln -s $PWD/start_webui.sh /usr/local/bin/start_webui.sh
 
